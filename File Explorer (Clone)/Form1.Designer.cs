a3888617d6788203c2193cb9133dc391a191595b
Rodrigo-200 <84940356+Rodrigo-200@users.noreply.github.com>
2024-10-23 23:15:20 +0100

    Adicionado   --> Botam para andar um nivel atras, para frente numa ação, para tras(n funcional)   --> Mudar o view type   --> Alterar o caminho   --> Adicionadas opções ao carrear com o botão direito

--- File Explorer (Clone)/Form1.Designer.cs
+++ File Explorer (Clone)/Form1.Designer.cs
@@ -1,15 +1,17 @@
-﻿
+﻿using System.Drawing;
+using System.Windows.Forms;
+
 namespace File_Explorer__Clone_
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,30 +26,71 @@ namespace File_Explorer__Clone_
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.lvw_FileExplorer = new System.Windows.Forms.ListView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.txt_Path = new System.Windows.Forms.TextBox();
+            this.lvw_FileExplorer = new System.Windows.Forms.ListView();
+            this.ch_Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_Modified = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cms_GeneralOptions = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewTextFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cbb_ViewType = new System.Windows.Forms.ComboBox();
+            this.txt_Path = new System.Windows.Forms.TextBox();
+            this.btn_GoUpOneLevel = new System.Windows.Forms.Button();
+            this.btn_Foward = new System.Windows.Forms.Button();
             this.btn_GoBack = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.apagarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.cms_GeneralOptions.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lvw_FileExplorer
+            // splitContainer1
             // 
-            this.lvw_FileExplorer.HideSelection = false;
-            this.lvw_FileExplorer.LargeImageList = this.imageList1;
-            this.lvw_FileExplorer.Location = new System.Drawing.Point(184, 90);
-            this.lvw_FileExplorer.Name = "lvw_FileExplorer";
-            this.lvw_FileExplorer.Size = new System.Drawing.Size(604, 348);
-            this.lvw_FileExplorer.TabIndex = 0;
-            this.lvw_FileExplorer.UseCompatibleStateImageBehavior = false;
-            this.lvw_FileExplorer.DoubleClick += new System.EventHandler(this.lvw_FileExplorer_DoubleClick);
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lvw_FileExplorer);
+            this.splitContainer1.Panel2.Controls.Add(this.cbb_ViewType);
+            this.splitContainer1.Panel2.Controls.Add(this.txt_Path);
+            this.splitContainer1.Panel2.Controls.Add(this.btn_GoUpOneLevel);
+            this.splitContainer1.Panel2.Controls.Add(this.btn_Foward);
+            this.splitContainer1.Panel2.Controls.Add(this.btn_GoBack);
+            this.splitContainer1.Size = new System.Drawing.Size(951, 558);
+            this.splitContainer1.SplitterDistance = 182;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(182, 558);
+            this.treeView1.StateImageList = this.imageList1;
+            this.treeView1.TabIndex = 0;
             // 
             // imageList1
             // 
@@ -55,65 +98,196 @@ namespace File_Explorer__Clone_
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             this.imageList1.Images.SetKeyName(0, "folder.png");
             this.imageList1.Images.SetKeyName(1, "document.png");
-            this.imageList1.Images.SetKeyName(2, "folder (1).png");
-            this.imageList1.Images.SetKeyName(3, "hidden.png");
             // 
-            // txt_Path
+            // lvw_FileExplorer
             // 
-            this.txt_Path.Location = new System.Drawing.Point(121, 12);
-            this.txt_Path.Name = "txt_Path";
-            this.txt_Path.ReadOnly = true;
-            this.txt_Path.Size = new System.Drawing.Size(667, 22);
-            this.txt_Path.TabIndex = 1;
+            this.lvw_FileExplorer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ch_Name,
+            this.ch_Modified,
+            this.ch_Type,
+            this.ch_Size});
+            this.lvw_FileExplorer.ContextMenuStrip = this.cms_GeneralOptions;
+            this.lvw_FileExplorer.FullRowSelect = true;
+            this.lvw_FileExplorer.HideSelection = false;
+            this.lvw_FileExplorer.LabelEdit = true;
+            this.lvw_FileExplorer.LargeImageList = this.imageList1;
+            this.lvw_FileExplorer.Location = new System.Drawing.Point(8, 63);
+            this.lvw_FileExplorer.Name = "lvw_FileExplorer";
+            this.lvw_FileExplorer.Size = new System.Drawing.Size(760, 491);
+            this.lvw_FileExplorer.SmallImageList = this.imageList1;
+            this.lvw_FileExplorer.TabIndex = 6;
+            this.lvw_FileExplorer.UseCompatibleStateImageBehavior = false;
+            this.lvw_FileExplorer.View = System.Windows.Forms.View.Details;
+            this.lvw_FileExplorer.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.lvw_FileExplorer_AfterLabelEdit);
+            this.lvw_FileExplorer.DoubleClick += new System.EventHandler(this.lvw_FileExplorer_DoubleClick);
+            this.lvw_FileExplorer.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lvw_FileExplorer_MouseClick);
+            // 
+            // ch_Name
+            // 
+            this.ch_Name.Text = "Name";
+            this.ch_Name.Width = 304;
+            // 
+            // ch_Modified
+            // 
+            this.ch_Modified.Text = "Date Modified";
+            this.ch_Modified.Width = 154;
+            // 
+            // ch_Type
+            // 
+            this.ch_Type.Text = "Type";
+            this.ch_Type.Width = 123;
+            // 
+            // ch_Size
+            // 
+            this.ch_Size.Text = "Size";
+            this.ch_Size.Width = 85;
+            // 
+            // cms_GeneralOptions
+            // 
+            this.cms_GeneralOptions.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem});
+            this.cms_GeneralOptions.Name = "cms_GeneralOptions";
+            this.cms_GeneralOptions.Size = new System.Drawing.Size(97, 26);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewTextFileToolStripMenuItem,
+            this.addNewFolderToolStripMenuItem});
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            // 
+            // addNewTextFileToolStripMenuItem
+            // 
+            this.addNewTextFileToolStripMenuItem.Image = global::File_Explorer__Clone_.Properties.Resources.document;
+            this.addNewTextFileToolStripMenuItem.Name = "addNewTextFileToolStripMenuItem";
+            this.addNewTextFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addNewTextFileToolStripMenuItem.Text = "Add new Text File";
+            this.addNewTextFileToolStripMenuItem.Click += new System.EventHandler(this.addNewTextFileToolStripMenuItem_Click);
+            // 
+            // addNewFolderToolStripMenuItem
+            // 
+            this.addNewFolderToolStripMenuItem.Image = global::File_Explorer__Clone_.Properties.Resources.folder;
+            this.addNewFolderToolStripMenuItem.Name = "addNewFolderToolStripMenuItem";
+            this.addNewFolderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addNewFolderToolStripMenuItem.Text = "Add New Folder";
+            this.addNewFolderToolStripMenuItem.Click += new System.EventHandler(this.addNewFolderToolStripMenuItem_Click);
             // 
             // cbb_ViewType
             // 
+            this.cbb_ViewType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cbb_ViewType.FormattingEnabled = true;
             this.cbb_ViewType.Items.AddRange(new object[] {
-            "Detalhes",
-            "Lista",
-            "Icons Grandes"});
-            this.cbb_ViewType.Location = new System.Drawing.Point(323, 60);
+            "Details",
+            "List",
+            "Large Icons",
+            "Small Icons"});
+            this.cbb_ViewType.Location = new System.Drawing.Point(666, 36);
             this.cbb_ViewType.Name = "cbb_ViewType";
-            this.cbb_ViewType.Size = new System.Drawing.Size(102, 24);
-            this.cbb_ViewType.TabIndex = 2;
+            this.cbb_ViewType.Size = new System.Drawing.Size(97, 21);
+            this.cbb_ViewType.TabIndex = 5;
             this.cbb_ViewType.SelectedIndexChanged += new System.EventHandler(this.cbb_ViewType_SelectedIndexChanged);
+            // 
+            // txt_Path
+            // 
+            this.txt_Path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Path.Location = new System.Drawing.Point(103, 11);
+            this.txt_Path.Name = "txt_Path";
+            this.txt_Path.Size = new System.Drawing.Size(669, 20);
+            this.txt_Path.TabIndex = 4;
+            this.txt_Path.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Path_KeyPress);
+            // 
+            // btn_GoUpOneLevel
+            // 
+            this.btn_GoUpOneLevel.FlatAppearance.BorderSize = 0;
+            this.btn_GoUpOneLevel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_GoUpOneLevel.Location = new System.Drawing.Point(71, 10);
+            this.btn_GoUpOneLevel.Name = "btn_GoUpOneLevel";
+            this.btn_GoUpOneLevel.Size = new System.Drawing.Size(27, 20);
+            this.btn_GoUpOneLevel.TabIndex = 3;
+            this.btn_GoUpOneLevel.Text = "↑";
+            this.btn_GoUpOneLevel.UseVisualStyleBackColor = true;
+            this.btn_GoUpOneLevel.Click += new System.EventHandler(this.btn_GoUpOneLevel_Click);
+            // 
+            // btn_Foward
+            // 
+            this.btn_Foward.FlatAppearance.BorderSize = 0;
+            this.btn_Foward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Foward.Location = new System.Drawing.Point(39, 10);
+            this.btn_Foward.Name = "btn_Foward";
+            this.btn_Foward.Size = new System.Drawing.Size(27, 20);
+            this.btn_Foward.TabIndex = 2;
+            this.btn_Foward.Text = "→";
+            this.btn_Foward.UseVisualStyleBackColor = true;
+            this.btn_Foward.Click += new System.EventHandler(this.btn_Foward_Click);
             // 
             // btn_GoBack
             // 
-            this.btn_GoBack.BackColor = System.Drawing.SystemColors.Control;
-            this.btn_GoBack.Location = new System.Drawing.Point(0, 13);
+            this.btn_GoBack.FlatAppearance.BorderSize = 0;
+            this.btn_GoBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_GoBack.Location = new System.Drawing.Point(8, 10);
             this.btn_GoBack.Name = "btn_GoBack";
-            this.btn_GoBack.Size = new System.Drawing.Size(48, 23);
-            this.btn_GoBack.TabIndex = 3;
-            this.btn_GoBack.Text = "<-";
-            this.btn_GoBack.UseVisualStyleBackColor = false;
+            this.btn_GoBack.Size = new System.Drawing.Size(27, 20);
+            this.btn_GoBack.TabIndex = 1;
+            this.btn_GoBack.Text = "←";
+            this.btn_GoBack.UseVisualStyleBackColor = true;
             this.btn_GoBack.Click += new System.EventHandler(this.btn_GoBack_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.apagarToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(113, 26);
+            // 
+            // apagarToolStripMenuItem
+            // 
+            this.apagarToolStripMenuItem.Name = "apagarToolStripMenuItem";
+            this.apagarToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.apagarToolStripMenuItem.Text = "Apagar";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btn_GoBack);
-            this.Controls.Add(this.cbb_ViewType);
-            this.Controls.Add(this.txt_Path);
-            this.Controls.Add(this.lvw_FileExplorer);
+            this.ClientSize = new System.Drawing.Size(951, 558);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.cms_GeneralOptions.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListView lvw_FileExplorer;
-        private System.Windows.Forms.TextBox txt_Path;
-        private System.Windows.Forms.ImageList imageList1;
-        private System.Windows.Forms.ComboBox cbb_ViewType;
-        private System.Windows.Forms.Button btn_GoBack;
+        private SplitContainer splitContainer1;
+        private Button btn_GoBack;
+        private TextBox txt_Path;
+        private Button btn_GoUpOneLevel;
+        private Button btn_Foward;
+        private ComboBox cbb_ViewType;
+        private TreeView treeView1;
+        private ImageList imageList1;
+        private ListView lvw_FileExplorer;
+        private ColumnHeader ch_Name;
+        private ColumnHeader ch_Modified;
+        private ToolStripMenuItem addToolStripMenuItem;
+        private ToolStripMenuItem addNewTextFileToolStripMenuItem;
+        private ToolStripMenuItem addNewFolderToolStripMenuItem;
+        private ContextMenuStrip cms_GeneralOptions;
+        private ColumnHeader ch_Type;
+        private ColumnHeader ch_Size;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem apagarToolStripMenuItem;
     }
 }
-
--- File Explorer (Clone)/Form1.cs
+++ File Explorer (Clone)/Form1.cs
@@ -13,8 +13,10 @@ namespace File_Explorer__Clone_
 {
     public partial class Form1 : Form
     {
-        string path = @"c:\";
-        
+        string path = Environment.CurrentDirectory + @"\";
+        string recent = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        List<string> OldPosition = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +25,9 @@ namespace File_Explorer__Clone_
         private void Form1_Load(object sender, EventArgs e)
         {
             txt_Path.Text = path;
+            lvw_FileExplorer.View = View.Details;
             RefreshExplorer();
+
         }
 
         private void RefreshExplorer()
@@ -35,56 +39,255 @@ namespace File_Explorer__Clone_
             {
                 try
                 {
-                        lvw_FileExplorer.Items.Add(new ListViewItem(item.Name, 0));   
+                    ListViewItem Directory = new ListViewItem();
+                    Directory.Text = item.Name;
+                    Directory.ImageIndex = 0;
+
+
+                    ListViewItem.ListViewSubItem Mod_date = new ListViewItem.ListViewSubItem();
+                    Mod_date.Text = item.LastWriteTime.ToString();
+                    ListViewItem.ListViewSubItem _Type = new ListViewItem.ListViewSubItem();
+                    _Type.Text = item.Extension.ToString();
+
+
+                    Directory.SubItems.Add(Mod_date);
+                    Directory.SubItems.Add(_Type);
+
+                    lvw_FileExplorer.Items.Add(Directory);
                 }
                 catch (Exception)
                 {
 
                     throw;
                 }
-
             }
             foreach (var item in dir.GetFiles()) // Vai a cada diretorio dentro do diretorio "path" e vai buscar todos os ficheiros
             {
-                lvw_FileExplorer.Items.Add(new ListViewItem(item.Name, 1));
+                ListViewItem listViewItem = new ListViewItem();
+                listViewItem.Text = item.Name;
+                listViewItem.ImageIndex = 1;
+
+                ListViewItem.ListViewSubItem Mod_date = new ListViewItem.ListViewSubItem();
+                Mod_date.Text = item.LastWriteTime.ToString();
+                ListViewItem.ListViewSubItem _Type = new ListViewItem.ListViewSubItem();
+                _Type.Text = item.Extension.ToString();
+                ListViewItem.ListViewSubItem Size = new ListViewItem.ListViewSubItem();
+                double ByteSize = Convert.ToDouble(item.Length);
+                Size.Text = (ByteSize / 1024).ToString();
+
+
+                listViewItem.SubItems.Add(Mod_date);
+                listViewItem.SubItems.Add(_Type);
+                listViewItem.SubItems.Add(Size);
+
+                lvw_FileExplorer.Items.Add(listViewItem);
             }
+
+
+
         }
 
         private void lvw_FileExplorer_DoubleClick(object sender, EventArgs e)
         {
-            try
+            if (lvw_FileExplorer.SelectedItems[0].ImageIndex == 0)
             {
+                try
+                {
                     path = path += lvw_FileExplorer.SelectedItems[0].Text + @"\";
                     RefreshExplorer();
-            }
-            catch (Exception)
-            {
+                }
+                catch (Exception)
+                {
 
-                throw;
+                    throw;
+                }
+            }
+            else
+            {
+                File.Open(path + @"\" + lvw_FileExplorer.SelectedItems[0].Text, FileMode.Open);
             }
 
         }
 
         private void btn_GoBack_Click(object sender, EventArgs e)
         {
-            path = new DirectoryInfo(path).Parent.FullName;
+            path = OldPosition.Last();
+            OldPosition.Remove(path);
+            OldPosition.Add(path);
+            
             RefreshExplorer();
         }
 
         private void cbb_ViewType_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(cbb_ViewType.SelectedItem == "Detalhes")
+            if (cbb_ViewType.SelectedItem == "Details")
             {
                 lvw_FileExplorer.View = View.Details;
             }
-            if (cbb_ViewType.SelectedItem == "Lista")
+            if (cbb_ViewType.SelectedItem == "List")
             {
                 lvw_FileExplorer.View = View.List;
             }
-            if (cbb_ViewType.SelectedItem == "Icons Grandes")
+            if (cbb_ViewType.SelectedItem == "Large Icons")
             {
                 lvw_FileExplorer.View = View.LargeIcon;
             }
+            if (cbb_ViewType.SelectedItem == "Small Icons")
+            {
+                lvw_FileExplorer.View = View.SmallIcon;
+            }
+        }
+
+
+        private void lvw_FileExplorer_AfterLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(e.Label))
+            {
+                e.CancelEdit = true;
+                MessageBox.Show("The file name cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string originalFullPath = Path.Combine(path, lvw_FileExplorer.Items[e.Item].Text);
+            string newFullPath = Path.Combine(path, e.Label);
+
+           
+            try
+            {
+                if (lvw_FileExplorer.FocusedItem.ImageIndex == 1)
+                {
+                    System.IO.File.Move(originalFullPath, newFullPath);
+                    lvw_FileExplorer.SelectedItems[0].Text = e.Label;
+
+                }
+                else
+                {
+                    System.IO.Directory.Move(lvw_FileExplorer.SelectedItems[0].Text, e.Label);
+                    lvw_FileExplorer.SelectedItems[0].Text = e.Label;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error renaming file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.CancelEdit = true;
+            }
+        }
+
+        private void btn_Foward_Click(object sender, EventArgs e)
+        {
+            path = OldPosition.Last();
+            OldPosition.Remove(path);
+            RefreshExplorer();
+        }
+
+        private void txt_Path_TextChanged(object sender, EventArgs e)
+        {
+            path = txt_Path.Text;
+        }
+
+
+        private void txt_Path_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                string oldpath = path;
+                path = txt_Path.Text;
+
+                OldPosition.Add(oldpath);
+                RefreshExplorer();
+            }
+        }
+
+        private void lvw_FileExplorer_MouseClick(object sender, MouseEventArgs e)
+        {
+            /*
+             * POR ACABAR
+             */
+            if (lvw_FileExplorer.FocusedItem != null)
+            {
+                if (e.Button == MouseButtons.Right)
+                {
+                    lvw_FileExplorer.ContextMenuStrip = contextMenuStrip1;
+                    cms_GeneralOptions.Show();
+
+                }
+            }
+            else
+            {
+                if (e.Button == MouseButtons.Right)
+                {
+                    lvw_FileExplorer.ContextMenuStrip = cms_GeneralOptions;
+                    cms_GeneralOptions.Show();
+                }
+            }
+
+        }
+
+        private void btn_GoUpOneLevel_Click(object sender, EventArgs e)
+        {
+            OldPosition.Add(path);
+            path = new DirectoryInfo(path).Parent.FullName + @"\";
+            RefreshExplorer();
+        }
+
+        private void addNewTextFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string newFileName = "New File.txt";
+
+            string fullPath = Path.Combine(path, newFileName);
+
+            try
+            {
+                using (FileStream fs = File.Create(fullPath)) { }
+
+                RefreshExplorer();
+
+
+                foreach (ListViewItem item in lvw_FileExplorer.Items)
+                {
+                    if (item.Text == newFileName)
+                    {
+                        lvw_FileExplorer.FocusedItem = item;
+                        item.Selected = true;
+                        item.BeginEdit();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating file: " + ex.Message);
+            }
+        }
+
+        private void addNewFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string newFolderName = "New Folder";
+
+            try
+            {
+                Directory.CreateDirectory(path + @"\" + newFolderName);
+
+                RefreshExplorer();
+
+
+                foreach (ListViewItem item in lvw_FileExplorer.Items)
+                {
+                    if (item.Text == newFolderName)
+                    {
+                        lvw_FileExplorer.FocusedItem = item;
+                        item.Selected = true;
+                        item.BeginEdit();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating Directory: " + ex.Message);
+            }
         }
     }
+
 }
+
